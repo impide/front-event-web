0c5e9c3d15413ed4db8a68fe259e50dfca02cd34
Tony Petrasinovic <tonypetrasinovic@gmail.com>
2024-05-12 16:31:21 +0200

    faq + formmodule

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  questions = [
+    {
+      title: 'Comment puis-je acheter des billets pour un événement ?',
+      answer:
+        'Vous pouvez acheter des billets en naviguant vers la page de l\'événement et en cliquant sur le bouton "Acheter des billets". Vous serez ensuite guidé à travers le processus de paiement.',
+      isOpen: false,
+    },
+    {
+      title: 'Comment puis-je obtenir un remboursement pour mon billet ?',
+      answer:
+        "Les remboursements dépendent de la politique de l'événement. Veuillez consulter la page de l'événement pour plus d'informations ou contactez-nous directement.",
+      isOpen: false,
+    },
+    {
+      title: 'Comment puis-je savoir si un événement est annulé ?',
+      answer:
+        "Les annulations d'événements seront annoncées sur la page de l'événement et les détenteurs de billets seront informés par email.",
+      isOpen: false,
+    },
+    {
+      title:
+        "Puis-je vendre mes billets si je ne peux pas assister à l'événement ?",
+      answer:
+        "Nous n'offrons pas actuellement de plateforme pour la revente de billets. Veuillez vérifier la politique de l'événement pour plus d'informations.",
+      isOpen: false,
+    },
+  ];
 
+  toggle(index: number) {
+    this.questions[index].isOpen = !this.questions[index].isOpen;
+  }
 }
--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  protected readonly form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
 
+  onSubmit() {
+    console.log('Form validity:', this.form.valid);
+    console.log('Form errors:', this.form.errors);
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
